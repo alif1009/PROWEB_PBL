dadb7285f1a1e381640d37182f26d97304c3e7fe
alif1009 <lifrizqulah@gamil.com>
2025-03-23 05:05:26 +0700

    menyiapkan list item dan dashboard

--- app/Http/Controllers/ListItemsController.php
+++ app/Http/Controllers/ListItemsController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ListItemController extends Controller
+class ListItemsController extends Controller
 {
     function tampilkan($id, $nama){
         return view('list_item', ['id' => $id, 'nama' => $nama]);
     }
 }
-
